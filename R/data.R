983b7d9aa3dec885d9c47e2aedde05b1a9258ac6
SorteHans <s224987@dtu.dk>
2024-10-29 09:33:07 +0100

    This is the updated master

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+#'
+#' Description
+#' This is a collection of all the possible codons and their equivalent amino acids
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
